75af3175039e2bc12041ebcb48b43cdd61737390
Lawrence <lgnjenga@gmail.com>
2024-01-29 13:15:24 +0300

    Python Variables, Comments(Single & Multiline), types and type casting

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -1,15 +1,55 @@
-# A variable is a container for a value, which can be of various types
+# Single line comment
 
 '''
-This is a 
-multiline comment
-or docstring (used to define a functions purpose)
-can be single or double quotes
+This is a multiline comment
+Can be single or double quotes
 '''
+"""
+Multiline comments are used for docstring
+docstring is used to define a function's Purpose
+"""
 
 """
+Variable: container for a value, can be of various types
 VARIABLE RULES:
-  - Variable names are case sensitive (name and NAME are different variables)
-  - Must start with a letter or an underscore
-  - Can have numbers but can not start with one
+    - Variables are case sensitive e.g. (name and NAME are two different variables)
+    - Must start with a letter of underscore
+    - can have numbers but not start with them    
 """
+
+# Single Assignmet of Variables
+# x = 5            # int
+# y = 4.8          # float
+# name = 'Gatonye' # str
+# is_on = False    # bool
+
+# Multiple assignments of Variables
+x, y, name, is_on = (5, 4.8, 'Gatonye', False)
+
+
+# Basic Math
+sum = x + y
+
+# Output the Variables
+print(x, y, name, is_on, sum)
+
+# Types and Type Casting
+# Type Casting: convert one datatype to another
+# int to str
+x = str(x) 
+print(type(x), x)
+
+y = int(y)
+print(type(y), y)
+
+z = float(y)
+print(type(z), z)
+
+
+
+
+
+
+
+
+
